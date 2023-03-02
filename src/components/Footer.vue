299d25f29b4da09b75d1a67ac17d2bdbc1e62120
Wangguibin <864562082@qq.com>
2023-03-02 21:48:52 +0800

    init code

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="main-footer sticky footer-type-1">
+    <div class="footer-inner">
+      <!-- 版权信息 -->
+      <div class="footer-text">
+        &copy; 2023-2025
+        <a href="#"><strong>WebStack</strong></a>
+        create by
+        <a href="http://github.com/WangGuibin" target="_blank"
+          ><strong>CoderWGB</strong></a
+        >
+      </div>
+
+      <div class="go-up">
+        <a href="#" rel="go-top"><i class="fa-angle-up"></i></a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+</style>
